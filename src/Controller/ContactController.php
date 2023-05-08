361f9d3675a7ba05a0748ce974389c2acc408d7a
Isaac Ghorzi <ghorzi.isaac@gmail.com>
2023-05-08 12:41:49 +0200

    ajout de la page de details des contacts

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,13 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contacts', name: 'app_contacts')]
+    #[Route('/contacts', name: 'app_contacts', methods: 'GET')]
     public function listContact(ContactRepository $repo): Response
     {
         $contacts = $repo->findAll();
         return $this->render('contact/listeContacts.html.twig', [
             'controller_name' => 'ContactController',
             'lesContacts' => $contacts
+        ]);
+    }
+
+    #[Route('/contact/{id}', name: 'app_ficheContact', methods: 'GET')]
+    public function ficheContact(Contact $contact): Response
+    {
+        return $this->render('contact/ficheContact.html.twig', [
+            'controller_name' => 'ContactController',
+            'leContact' => $contact
         ]);
     }
 }
